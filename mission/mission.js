71019a75ce8ca857b7e05479facd29e34d9ad3b1
SonofPR1ME <sonofpr1memus@gmail.com>
2026-05-02 21:47:19 -0600

    Adjusted the code

--- mission/mission.js
+++ mission/mission.js
@@ -7,12 +7,16 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        document.querySelector('body').style.backgroundColor = 'black';
-        document.querySelector('body').style.color = 'white';
+        document.querySelector('body').classList.add('dark');
+        document.querySelector('#content').classList.add('dark');
+        document.querySelector('.dark').style.backgroundColor = 'black';
+        document.querySelector('.dark').style.color = 'white';
+        document.querySelector('.dark').style.border = '1px solid white';
         logo.setAttribute('src', 'images/byui-logo-white.png');
     } else {
         document.querySelector('body').style.backgroundColor = 'white';
         document.querySelector('body').style.color = 'black';
+        document.querySelector('#content').style.border = '1px solid black';
         logo.setAttribute('src', 'images/byui-logo-blue.webp');
     }
 }           
