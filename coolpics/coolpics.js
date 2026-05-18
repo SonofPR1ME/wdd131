fd8a04853f6a12767f901078b2579cd5a47a135b
SonofPR1ME <sonofpr1memus@gmail.com>
2026-05-17 22:02:31 -0600

    Did the prove assignment

---
+++ coolpics/coolpics.js
@@ -0,0 +1,41 @@
+
+const gallery = document.querySelector('.images');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    
+console.log(e.target);
+
+const img = e.target;
+const src = img.getAttribute('src');
+const alt = img.getAttribute('alt');
+const full = src.replace('sm', 'full');
+
+modalImage.src = full
+modalImage.alt = alt
+
+modal.showModal();
+
+}
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
+const btn = document.querySelector(".menu");
+const menu = document.querySelector("nav");
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    menu.classList.toggle('hide')
+}
