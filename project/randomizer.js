6803e398bcc7abdfc22fc0a1d672c0150363b22c
SonofPR1ME <sonofpr1memus@gmail.com>
2026-07-15 21:34:42 -0600

    Finished all the randomizers functionally

--- project/randomizer.js
+++ project/randomizer.js
@@ -1,5 +1,16 @@
 const hexidecimal = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
 
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+     "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+const nouns = ["cat", "dog", "cow", "boy", "girl", "apple", "car", "house", "tree", "burrito", 
+    "box", "dragon", "home", "horse", "zebra", "chimpanzee", "giraffe", "unicorn", "phone", 
+    "computer", "floor", "hole", "bat", "french fry", "taco"]
+
+const verbs = ["combine", "run", "cry", "eat", "sleep", "jump", "climb", "walk", "drive", 
+    "drink", "went", "play", "turn", "look", "scream", "say", "think", "read", "roar", "hide", 
+    "try", "catch", "kick", "steal", "buy", "cook", "grab", "mix"]
+
 let randomNumber = {
     min: 1,
     max: 10,
@@ -21,6 +32,23 @@ let randomGrid = {
     columnmax: 5,
     row: undefined,
     column: undefined,
+}
+
+let randomLetter = {
+    letter: undefined,
+}
+
+let randomList = {
+    originalList: [],
+    newList: [],
+}
+
+let randomNoun = {
+    noun: undefined,
+}
+
+let randomVerb = {
+    verb: undefined,
 }
 
 function generateRandomNumber() {
@@ -48,7 +76,38 @@ function generateRandomGrid() {
     randomGrid.columnmax = parseInt(document.querySelector("#columnmax").value) || 5
     randomGrid.row = Math.floor(Math.random() * randomGrid.rowmax) + 1;
     randomGrid.column = Math.floor(Math.random() * randomGrid.columnmax) + 1;
-    document.querySelector("#random-grid").innerHTML = "R" + randomGrid.row + "C" + randomGrid.column;
+    document.querySelector("#random-grid").innerHTML = "R" + randomGrid.row + "C" +
+     randomGrid.column;
+}
+
+function generateRandomLetter() {
+    randomLetter.letter = letters[Math.floor(Math.random() * letters.length)];
+    document.querySelector("#random-letter").innerHTML = randomLetter.letter;
+}
+
+function generateRandomList() {
+    randomList.originalList = document.querySelector("#original-list").value.split(", ");
+    randomList.newList = [];
+    let listNumber = randomList.originalList.length;
+    while (listNumber > 0)
+    {
+        let currentList = randomList.originalList.filter(item =>
+             !randomList.newList.includes(item));
+        let randomIndex = Math.floor(Math.random() * currentList.length);
+        randomList.newList.push(currentList[randomIndex]);
+        listNumber = listNumber - 1;
+    }
+    document.querySelector("#random-list").innerHTML = randomList.newList.join(", ");
+    }
+
+function generateRandomNoun() {
+    randomNoun.noun = nouns[Math.floor(Math.random() * nouns.length)];
+    document.querySelector("#random-noun").innerHTML = randomNoun.noun;
+}
+
+function generateRandomVerb() {
+    randomVerb.verb = verbs[Math.floor(Math.random() * verbs.length)];
+    document.querySelector("#random-verb").innerHTML = randomVerb.verb;
 }
 
 let numberBtn = document.querySelector("#number-btn");
@@ -59,3 +118,15 @@ colorBtn.addEventListener("click", generateRandomColor);
 
 let gridBtn = document.querySelector("#grid-btn");
 gridBtn.addEventListener("click", generateRandomGrid);
+
+let letterBtn = document.querySelector("#letter-btn");
+letterBtn.addEventListener("click", generateRandomLetter);
+
+let listBtn = document.querySelector("#list-btn");
+listBtn.addEventListener("click", generateRandomList);
+
+let nounBtn = document.querySelector("#noun-btn");
+nounBtn.addEventListener("click", generateRandomNoun);
+
+let verbBtn = document.querySelector("#verb-btn");
+verbBtn.addEventListener("click", generateRandomVerb);
